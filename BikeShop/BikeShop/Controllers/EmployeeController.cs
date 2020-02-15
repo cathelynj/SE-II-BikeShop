024e91e37ba42438a4341160049718cd44e3afa9
garrett-t <LANDONG@ETSU.EDU>
2020-02-15 16:10:47 -0500

    Controllers

---
+++ BikeShop/BikeShop/Controllers/EmployeeController.cs
@@ -0,0 +1,111 @@
+﻿using BikeShop.Models;
+using BikeShop.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BikeShop.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        public IHttpActionResult GetAllEmployees()
+        {
+            IList<EmployeeViewModel> employees = null;
+            using (var ctx = new BikeShopEntities())
+            {
+                employees = ctx.EMPLOYEEs.Select(e => new EmployeeViewModel()
+                {
+                    EmployeeID = e.EMPLOYEEID,
+                    TaxpayerID = e.TAXPAYERID,
+                    LastName = e.LASTNAME,
+                    FirstName = e.FIRSTNAME,
+                    HomePhone = e.HOMEPHONE,
+                    Address = e.ADDRESS,
+                    ZipCode = e.ZIPCODE,
+                    CityID = e.CITYID,
+                    DateHired = e.DATEHIRED,
+                    DateReleased = e.DATERELEASED,
+                    CurrentManager = e.CURRENTMANAGER,
+                    SalaryGrade = e.SALARYGRADE,
+                    Salary = e.SALARY,
+                    Title = e.TITLE,
+                    Workarea = e.WORKAREA
+                }).ToList<EmployeeViewModel>();
+            }
+            if (employees.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(employees);
+        }
+
+        public IHttpActionResult GetEmployee(int id)
+        {
+            EmployeeViewModel employee = null;
+
+            using (var ctx = new BikeShopEntities())
+            {
+                employee = ctx.EMPLOYEEs
+                    .Where(e => e.EMPLOYEEID == id)
+                    .Select(e => new EmployeeViewModel()
+                    {
+                        EmployeeID = e.EMPLOYEEID,
+                        TaxpayerID = e.TAXPAYERID,
+                        LastName = e.LASTNAME,
+                        FirstName = e.FIRSTNAME,
+                        HomePhone = e.HOMEPHONE,
+                        Address = e.ADDRESS,
+                        ZipCode = e.ZIPCODE,
+                        CityID = e.CITYID,
+                        DateHired = e.DATEHIRED,
+                        DateReleased = e.DATERELEASED,
+                        CurrentManager = e.CURRENTMANAGER,
+                        SalaryGrade = e.SALARYGRADE,
+                        Salary = e.SALARY,
+                        Title = e.TITLE,
+                        Workarea = e.WORKAREA
+                    }).FirstOrDefault<EmployeeViewModel>();
+            }
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        public IHttpActionResult PostNewEmployee(EmployeeViewModel employee)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            using (var ctx = new BikeShopEntities())
+            {
+                ctx.EMPLOYEEs.Add(new EMPLOYEE()
+                {
+                    EMPLOYEEID = employee.EmployeeID,
+                    TAXPAYERID = employee.TaxpayerID,
+                    LASTNAME = employee.LastName,
+                    FIRSTNAME = employee.FirstName,
+                    HOMEPHONE = employee.HomePhone,
+                    ADDRESS = employee.Address,
+                    ZIPCODE = employee.ZipCode,
+                    CITYID = employee.CityID,
+                    DATEHIRED = employee.DateHired,
+                    DATERELEASED = employee.DateReleased,
+                    CURRENTMANAGER = employee.CurrentManager,
+                    SALARYGRADE = employee.SalaryGrade,
+                    SALARY = employee.Salary,
+                    TITLE = employee.Title,
+                    WORKAREA = employee.Workarea
+                });
+
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
+    }
+}
---
+++ BikeShop/BikeShop/Models/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BikeShop.Models.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public int EmployeeID { get; set; }
+        public string TaxpayerID { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string HomePhone { get; set; }
+        public string Address { get; set; }
+        public string ZipCode { get; set; }
+        public Nullable<int> CityID { get; set; }
+        public Nullable<System.DateTime> DateHired { get; set; }
+        public Nullable<System.DateTime> DateReleased { get; set; }
+        public Nullable<int> CurrentManager { get; set; }
+        public Nullable<int> SalaryGrade { get; set; }
+        public Nullable<decimal> Salary { get; set; }
+        public string Title { get; set; }
+        public string Workarea { get; set; }
+    }
+}

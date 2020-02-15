13cb111b8ff9feeff84c7613ef8bab1c18fdd73e
garrett-t <LANDONG@ETSU.EDU>
2020-02-15 15:57:48 -0500

    Base Files

---
+++ BikeShop/BikeShop/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace BikeShop.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ BikeShop/BikeShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BikeShop.WebApiApplication" Language="C#" %>

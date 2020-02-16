4fb1b53758e7b477969c66479a0ff976b7631825
Casey Gregory <GREGORYCA1@ETSU.EDU>
2020-02-16 12:49:23 -0500

    I have added LetterStyle and GroupO to the tables that we can read

---
+++ BikeShop/BikeShop/Models/ViewModels/LetterStyleViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BikeShop.Models.ViewModels
+{
+    public class LetterStyleViewModel
+    {
+        public string LetterStyle1 { get; set; }
+        public string Description { get; set; }
+    }
+}

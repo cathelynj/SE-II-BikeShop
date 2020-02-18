a356d0dac60ad0b277606aa67f635dcfc2444a7c
Casey Gregory <GREGORYCA1@ETSU.EDU>
2020-02-18 17:29:04 -0500

    Added both the Paint and Preference GetAll Methods

---
+++ BikeShop/BikeShop/Controllers/api/PaintController.cs
@@ -0,0 +1,38 @@
+﻿using BikeShop.Models;
+using BikeShop.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BikeShop.Controllers.api
+{
+    public class PaintController : ApiController
+    {
+        public IHttpActionResult GetAllPaint()
+        {
+            IList<PaintViewModel> paint = null;
+            using (var ctx = new BikeShopEntities())
+            {
+                paint = ctx.PAINTs.Select(c => new PaintViewModel()
+                {
+                    PaintID = c.PAINTID,
+                    ColorName = c.COLORNAME,
+                    ColorStyle = c.COLORSTYLE,
+                    ColorList = c.COLORLIST,
+                    DateIntroduced = c.DATEINTRODUCED,
+                    DateDiscontinued = c.DATEDISCONTINUED
+
+    }).ToList<PaintViewModel>();
+            }
+            if (paint.Count == 0)
+            {
+                return NotFound();
+            }
+
+            return Ok(paint);
+        }
+    }
+}
---
+++ BikeShop/BikeShop/Controllers/api/PreferenceController.cs
@@ -0,0 +1,39 @@
+﻿using BikeShop.Models;
+using BikeShop.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BikeShop.Controllers.api
+{
+    public class PreferenceController : ApiController
+    {
+        public class GroupOController : ApiController
+        {
+            public IHttpActionResult GetAllGroupO()
+            {
+                IList<PreferenceViewModel> preference = null;
+                using (var ctx = new BikeShopEntities())
+                {
+                    preference = ctx.PREFERENCEs.Select(c => new PreferenceViewModel()
+                    {
+                        ItemName = c.ITEMNAME,
+                        Value = c.VALUE,
+                        Description = c.DESCRIPTION,
+                        DateChanged = c.DATECHANGED
+
+                    }).ToList<PreferenceViewModel>();
+                }
+                if (preference.Count == 0)
+                {
+                    return NotFound();
+                }
+
+                return Ok(preference);
+            }
+        }
+    }
+}

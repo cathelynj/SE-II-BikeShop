2c500199fb4b697fd79dd4e4a5bb35a21cf6a23a
garrett-t <LANDONG@ETSU.EDU>
2020-02-19 12:30:28 -0500

    ManufactorerTransactions PUT and DELETE

--- BikeShop/BikeShop/Controllers/ManufactorerTransactionsController.cs
+++ BikeShop/BikeShop/Controllers/ManufactorerTransactionsController.cs
@@ -65,13 +65,13 @@ namespace BikeShop.Controllers
 
         public ActionResult Edit(int id, System.DateTime date, int reference)
         {
-            ManufactorerTransactionViewModel mt = null;
+            ManufactorerTransactionViewModel manufactorerTransaction = null;
 
             using (var client = new HttpClient())
             {
                 client.BaseAddress = new Uri("http://localhost:51968/api/");
 
-                var responseTask = client.GetAsync("mt?id=" + id.ToString() + "&date=" + date.ToString() + "&reference=" + reference.ToString());
+                var responseTask = client.GetAsync("manufactorertransaction?id=" + id.ToString() + "&date=" + date.ToString() + "&reference=" + reference.ToString());
                 responseTask.Wait();
 
                 var result = responseTask.Result;
@@ -80,10 +80,47 @@ namespace BikeShop.Controllers
                     var readTask = result.Content.ReadAsAsync<ManufactorerTransactionViewModel>();
                     readTask.Wait();
 
-                    mt = readTask.Result;
+                    manufactorerTransaction = readTask.Result;
+                }
+            }
+            return View(manufactorerTransaction);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ManufactorerTransactionViewModel mt)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/manufactorertransaction");
+
+                var putTask = client.PutAsXmlAsync<ManufactorerTransactionViewModel>("manufactorertransaction", mt);
+                putTask.Wait();
+
+                var result = putTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
                 }
             }
             return View(mt);
+        }
+
+        public ActionResult Delete(int id, System.DateTime date, int reference)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/manufactorertransaction");
+
+                var deleteTask = client.DeleteAsync("manufactorertransaction/" + id.ToString() + "&" + date.ToString() + "&" + reference.ToString());
+                deleteTask.Wait();
+
+                var result = deleteTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return RedirectToAction("Index");
         }
     }
 }

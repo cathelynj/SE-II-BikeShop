0dfd33e6a9902eabb4a55857faaa6cabcb903941
garrett-t <LANDONG@ETSU.EDU>
2020-02-20 09:59:37 -0500

    samplename views and api methods

---
+++ BikeShop/BikeShop/Controllers/SampleNamesController.cs
@@ -0,0 +1,126 @@
+﻿using BikeShop.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BikeShop.Controllers
+{
+    public class SampleNamesController : Controller
+    {
+        // GET: SampleNames
+        public ActionResult Index()
+        {
+            IEnumerable<SampleNameViewModel> s = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/");
+                var responseTask = client.GetAsync("samplename");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<SampleNameViewModel>>();
+                    readTask.Wait();
+
+                    s = readTask.Result;
+                }
+                else
+                {
+                    s = Enumerable.Empty<SampleNameViewModel>();
+                    ModelState.AddModelError(string.Empty, "Server error.");
+                }
+            }
+            return View(s);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(SampleNameViewModel s)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/samplename");
+
+                var postTask = client.PostAsXmlAsync<SampleNameViewModel>("samplename", s);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Server error.");
+            return View(s);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            SampleNameViewModel s = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/");
+
+                var responseTask = client.GetAsync("samplename?id=" + id.ToString());
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<SampleNameViewModel>();
+                    readTask.Wait();
+
+                    s = readTask.Result;
+                }
+            }
+            return View(s);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(SampleNameViewModel s)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/samplename");
+
+                var putTask = client.PutAsXmlAsync<SampleNameViewModel>("samplename", s);
+                putTask.Wait();
+
+                var result = putTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(s);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/samplename");
+
+                var deleteTask = client.DeleteAsync("samplename/" + id.ToString());
+                deleteTask.Wait();
+
+                var result = deleteTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- BikeShop/BikeShop/Controllers/api/SampleNameController.cs
+++ BikeShop/BikeShop/Controllers/api/SampleNameController.cs
@@ -31,5 +31,87 @@ namespace BikeShop.Controllers.api
 
             return Ok(samplename);
         }
+
+        public IHttpActionResult GetSampleName(int id)
+        {
+            SampleNameViewModel sn = null;
+
+            using (var ctx = new BikeShopEntities())
+            {
+                sn = ctx.SAMPLENAMEs
+                    .Where(s => s.ID == id)
+                    .Select(s => new SampleNameViewModel()
+                    {
+                        ID = s.ID,
+                        FIRSTNAME = s.FIRSTNAME,
+                        LASTNAME = s.LASTNAME,
+                        GENDER = s.GENDER
+                    }).FirstOrDefault<SampleNameViewModel>();
+            }
+            if (sn == null)
+            {
+                return NotFound();
+            }
+            return Ok(sn);
+        }
+
+        public IHttpActionResult PostNewSampleName(SampleNameViewModel s)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            using (var ctx = new BikeShopEntities())
+            {
+                ctx.SAMPLENAMEs.Add(new SAMPLENAME()
+                {
+                    ID = s.ID,
+                    FIRSTNAME = s.FIRSTNAME,
+                    LASTNAME = s.LASTNAME,
+                    GENDER = s.GENDER
+                });
+
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Put(SampleNameViewModel s)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
+            using (var ctx = new BikeShopEntities())
+            {
+                var existingBP = ctx.SAMPLENAMEs.Where(w => w.ID == s.ID).FirstOrDefault<SAMPLENAME>();
+                if (existingBP != null)
+                {
+                    existingBP.ID = s.ID;
+                    existingBP.FIRSTNAME = s.FIRSTNAME;
+                    existingBP.LASTNAME = s.LASTNAME;
+                    existingBP.GENDER = s.GENDER;
+
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+
+            using (var ctx = new BikeShopEntities())
+            {
+                var bp = ctx.SAMPLENAMEs
+                    .Where(w => w.ID == id)
+                    .FirstOrDefault();
+
+                ctx.Entry(bp).State = System.Data.Entity.EntityState.Deleted;
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
     }
 }

3466d60868ec1aa3a462600045a080cee1b592fe
garrett-t <LANDONG@ETSU.EDU>
2020-02-20 11:25:04 -0500

    tubematerial views and api methods

--- BikeShop/BikeShop/Controllers/api/TubeMaterialController.cs
+++ BikeShop/BikeShop/Controllers/api/TubeMaterialController.cs
@@ -37,5 +37,105 @@ namespace BikeShop.Controllers.api
 
             return Ok(tubematerial);
         }
+
+        public IHttpActionResult GetTubeMaterial(int id)
+        {
+            TubeMaterialViewModel tm = null;
+
+            using (var ctx = new BikeShopEntities())
+            {
+                tm = ctx.TUBEMATERIALs
+                    .Where(t => t.TUBEID == id)
+                    .Select(t => new TubeMaterialViewModel()
+                    {
+                        TUBEID = t.TUBEID,
+                        MATERIAL = t.MATERIAL,
+                        DESCRIPTION = t.DESCRIPTION,
+                        DIAMETER = t.DIAMETER,
+                        THICKNESS = t.THICKNESS,
+                        ROUNDNESS = t.ROUNDNESS,
+                        WEIGHT = t.WEIGHT,
+                        STIFFNESS = t.STIFFNESS,
+                        LISTPRICE = t.LISTPRICE,
+                        CONSTRUCTION = t.CONSTRUCTION
+                    }).FirstOrDefault<TubeMaterialViewModel>();
+            }
+            if (tm == null)
+            {
+                return NotFound();
+            }
+            return Ok(tm);
+        }
+
+        public IHttpActionResult PostNewTubeMaterial(TubeMaterialViewModel t)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            using (var ctx = new BikeShopEntities())
+            {
+                ctx.TUBEMATERIALs.Add(new TUBEMATERIAL()
+                {
+                    TUBEID = t.TUBEID,
+                    MATERIAL = t.MATERIAL,
+                    DESCRIPTION = t.DESCRIPTION,
+                    DIAMETER = t.DIAMETER,
+                    THICKNESS = t.THICKNESS,
+                    ROUNDNESS = t.ROUNDNESS,
+                    WEIGHT = t.WEIGHT,
+                    STIFFNESS = t.STIFFNESS,
+                    LISTPRICE = t.LISTPRICE,
+                    CONSTRUCTION = t.CONSTRUCTION
+                });
+
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Put(TubeMaterialViewModel t)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
+            using (var ctx = new BikeShopEntities())
+            {
+                var existingBP = ctx.TUBEMATERIALs.Where(w => w.TUBEID == t.TUBEID).FirstOrDefault<TUBEMATERIAL>();
+                if (existingBP != null)
+                {
+                    existingBP.TUBEID = t.TUBEID;
+                    existingBP.MATERIAL = t.MATERIAL;
+                    existingBP.DESCRIPTION = t.DESCRIPTION;
+                    existingBP.DIAMETER = t.DIAMETER;
+                    existingBP.THICKNESS = t.THICKNESS;
+                    existingBP.ROUNDNESS = t.ROUNDNESS;
+                    existingBP.WEIGHT = t.WEIGHT;
+                    existingBP.STIFFNESS = t.STIFFNESS;
+                    existingBP.LISTPRICE = t.LISTPRICE;
+                    existingBP.CONSTRUCTION = t.CONSTRUCTION;
+
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+
+            using (var ctx = new BikeShopEntities())
+            {
+                var bp = ctx.TUBEMATERIALs
+                    .Where(w => w.TUBEID == id)
+                    .FirstOrDefault();
+
+                ctx.Entry(bp).State = System.Data.Entity.EntityState.Deleted;
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
     }
 }

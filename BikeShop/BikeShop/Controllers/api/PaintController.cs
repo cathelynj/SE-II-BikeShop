49905902c2e86d643b491aefe760b814c70a5880
garrett-t <LANDONG@ETSU.EDU>
2020-02-19 23:17:49 -0500

    Paint views and api methods

--- BikeShop/BikeShop/Controllers/api/PaintController.cs
+++ BikeShop/BikeShop/Controllers/api/PaintController.cs
@@ -25,7 +25,7 @@ namespace BikeShop.Controllers.api
                     DateIntroduced = c.DATEINTRODUCED,
                     DateDiscontinued = c.DATEDISCONTINUED
 
-    }).ToList<PaintViewModel>();
+                }).ToList<PaintViewModel>();
             }
             if (paint.Count == 0)
             {
@@ -33,6 +33,94 @@ namespace BikeShop.Controllers.api
             }
 
             return Ok(paint);
+        }
+
+        public IHttpActionResult GetPaint(int id)
+        {
+            PaintViewModel p = null;
+
+            using (var ctx = new BikeShopEntities())
+            {
+                p = ctx.PAINTs
+                    .Where(c => c.PAINTID == id)
+                    .Select(c => new PaintViewModel()
+                    {
+                        PaintID = c.PAINTID,
+                        ColorName = c.COLORNAME,
+                        ColorStyle = c.COLORSTYLE,
+                        ColorList = c.COLORLIST,
+                        DateIntroduced = c.DATEINTRODUCED,
+                        DateDiscontinued = c.DATEDISCONTINUED
+                    }).FirstOrDefault<PaintViewModel>();
+            }
+            if (p == null)
+            {
+                return NotFound();
+            }
+            return Ok(p);
+        }
+
+        public IHttpActionResult PostNewPaint(PaintViewModel c)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            using (var ctx = new BikeShopEntities())
+            {
+                ctx.PAINTs.Add(new PAINT()
+                {
+                    PAINTID = c.PaintID,
+                    COLORNAME = c.ColorName,
+                    COLORSTYLE = c.ColorStyle,
+                    COLORLIST = c.ColorList,
+                    DATEINTRODUCED = c.DateIntroduced,
+                    DATEDISCONTINUED = c.DateDiscontinued
+                });
+
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Put(PaintViewModel c)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
+            using (var ctx = new BikeShopEntities())
+            {
+                var existingBP = ctx.PAINTs.Where(w => w.PAINTID == c.PaintID).FirstOrDefault<PAINT>();
+                if (existingBP != null)
+                {
+                    existingBP.PAINTID = c.PaintID;
+                    existingBP.COLORNAME = c.ColorName;
+                    existingBP.COLORSTYLE = c.ColorStyle;
+                    existingBP.COLORLIST = c.ColorList;
+                    existingBP.DATEINTRODUCED = c.DateIntroduced;
+                    existingBP.DATEDISCONTINUED = c.DateDiscontinued;
+
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+
+            using (var ctx = new BikeShopEntities())
+            {
+                var bp = ctx.PAINTs
+                    .Where(w => w.PAINTID == id)
+                    .FirstOrDefault();
+
+                ctx.Entry(bp).State = System.Data.Entity.EntityState.Deleted;
+                ctx.SaveChanges();
+            }
+            return Ok();
         }
     }
 }

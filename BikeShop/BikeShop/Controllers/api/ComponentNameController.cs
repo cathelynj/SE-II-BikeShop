80a30e372657ef2d500f8697d64a7efdc37447c3
garrett-t <LANDONG@ETSU.EDU>
2020-02-20 14:54:45 -0500

    componentname views and api methods

--- BikeShop/BikeShop/Controllers/api/ComponentNameController.cs
+++ BikeShop/BikeShop/Controllers/api/ComponentNameController.cs
@@ -30,5 +30,84 @@ namespace BikeShop.Controllers.api
 
             return Ok(componentname);
         }
+
+        public IHttpActionResult GetComponentName(string id)
+        {
+            ComponentNameViewModel cn = null;
+
+            using (var ctx = new BikeShopEntities())
+            {
+                cn = ctx.COMPONENTNAMEs
+                    .Where(c => c.COMPONENTNAME1 == id)
+                    .Select(c => new ComponentNameViewModel()
+                    {
+                        COMPONENTNAME1 = c.COMPONENTNAME1,
+                        ASSEMBLYORDER = c.ASSEMBLYORDER,
+                        DESCRIPTION = c.DESCRIPTION
+                    }).FirstOrDefault<ComponentNameViewModel>();
+            }
+            if (cn == null)
+            {
+                return NotFound();
+            }
+            return Ok(cn);
+        }
+
+        public IHttpActionResult PostNewBikePart(ComponentNameViewModel c)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            using (var ctx = new BikeShopEntities())
+            {
+                ctx.COMPONENTNAMEs.Add(new COMPONENTNAME()
+                {
+                    COMPONENTNAME1 = c.COMPONENTNAME1,
+                    ASSEMBLYORDER = c.ASSEMBLYORDER,
+                    DESCRIPTION = c.DESCRIPTION
+                });
+
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Put(ComponentNameViewModel c)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
+            using (var ctx = new BikeShopEntities())
+            {
+                var existingBP = ctx.COMPONENTNAMEs.Where(w => w.COMPONENTNAME1 == c.COMPONENTNAME1).FirstOrDefault<COMPONENTNAME>();
+                if (existingBP != null)
+                {
+                    existingBP.COMPONENTNAME1 = c.COMPONENTNAME1;
+                    existingBP.ASSEMBLYORDER = c.ASSEMBLYORDER;
+                    existingBP.DESCRIPTION = c.DESCRIPTION;
+
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(string id)
+        {
+
+            using (var ctx = new BikeShopEntities())
+            {
+                var bp = ctx.COMPONENTNAMEs
+                    .Where(w => w.COMPONENTNAME1 == id)
+                    .FirstOrDefault();
+
+                ctx.Entry(bp).State = System.Data.Entity.EntityState.Deleted;
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
     }
 }

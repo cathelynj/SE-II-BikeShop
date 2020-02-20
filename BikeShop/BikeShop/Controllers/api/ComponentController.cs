0bda32f202ce462928d6d7e08badec7af18dba3d
garrett-t <LANDONG@ETSU.EDU>
2020-02-19 21:18:20 -0500

    componets view and api methods

--- BikeShop/BikeShop/Controllers/api/ComponentController.cs
+++ BikeShop/BikeShop/Controllers/api/ComponentController.cs
@@ -42,5 +42,120 @@ namespace BikeShop.Controllers.api
 
             return Ok(components);
         }
+
+        public IHttpActionResult GetComponent(int id)
+        {
+            ComponentViewModel bikeParts = null;
+
+            using (var ctx = new BikeShopEntities())
+            {
+                bikeParts = ctx.COMPONENTs
+                    .Where(c => c.COMPONENTID == id)
+                    .Select(c => new ComponentViewModel()
+                    {
+                        COMPONENTID = c.COMPONENTID,
+                        MANUFACTURERID = c.MANUFACTURERID,
+                        PRODUCTNUMBER = c.PRODUCTNUMBER,
+                        ROAD = c.ROAD,
+                        CATEGORY = c.CATEGORY,
+                        LENGTH = c.LENGTH,
+                        HEIGHT = c.HEIGHT,
+                        WIDTH = c.WIDTH,
+                        WEIGHT = c.WEIGHT,
+                        YEAR = c.YEAR,
+                        ENDYEAR = c.ENDYEAR,
+                        DESCRIPTION = c.DESCRIPTION,
+                        LISTPRICE = c.LISTPRICE,
+                        ESTIMATEDCOST = c.ESTIMATEDCOST,
+                        QUANTITYONHAND = c.QUANTITYONHAND
+                    }).FirstOrDefault<ComponentViewModel>();
+            }
+            if (bikeParts == null)
+            {
+                return NotFound();
+            }
+            return Ok(bikeParts);
+        }
+
+        public IHttpActionResult PostNewComponent(ComponentViewModel c)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            using (var ctx = new BikeShopEntities())
+            {
+                ctx.COMPONENTs.Add(new COMPONENT()
+                {
+                    COMPONENTID = c.COMPONENTID,
+                    MANUFACTURERID = c.MANUFACTURERID,
+                    PRODUCTNUMBER = c.PRODUCTNUMBER,
+                    ROAD = c.ROAD,
+                    CATEGORY = c.CATEGORY,
+                    LENGTH = c.LENGTH,
+                    HEIGHT = c.HEIGHT,
+                    WIDTH = c.WIDTH,
+                    WEIGHT = c.WEIGHT,
+                    YEAR = c.YEAR,
+                    ENDYEAR = c.ENDYEAR,
+                    DESCRIPTION = c.DESCRIPTION,
+                    LISTPRICE = c.LISTPRICE,
+                    ESTIMATEDCOST = c.ESTIMATEDCOST,
+                    QUANTITYONHAND = c.QUANTITYONHAND
+                });
+
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Put(ComponentViewModel c)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
+            using (var ctx = new BikeShopEntities())
+            {
+                var existingC = ctx.COMPONENTs.Where(w => w.COMPONENTID == c.COMPONENTID).FirstOrDefault<COMPONENT>();
+                if (existingC != null)
+                {
+                    existingC.COMPONENTID = c.COMPONENTID;
+                    existingC.MANUFACTURERID = c.MANUFACTURERID;
+                    existingC.PRODUCTNUMBER = c.PRODUCTNUMBER;
+                    existingC.ROAD = c.ROAD;
+                    existingC.CATEGORY = c.CATEGORY;
+                    existingC.LENGTH = c.LENGTH;
+                    existingC.HEIGHT = c.HEIGHT;
+                    existingC.WIDTH = c.WIDTH;
+                    existingC.WEIGHT = c.WEIGHT;
+                    existingC.YEAR = c.YEAR;
+                    existingC.ENDYEAR = c.ENDYEAR;
+                    existingC.DESCRIPTION = c.DESCRIPTION;
+                    existingC.LISTPRICE = c.LISTPRICE;
+                    existingC.ESTIMATEDCOST = c.ESTIMATEDCOST;
+                    existingC.QUANTITYONHAND = c.QUANTITYONHAND;
+
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+
+            using (var ctx = new BikeShopEntities())
+            {
+                var bp = ctx.COMPONENTs
+                    .Where(w => w.COMPONENTID == id)
+                    .FirstOrDefault();
+
+                ctx.Entry(bp).State = System.Data.Entity.EntityState.Deleted;
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
     }
 }

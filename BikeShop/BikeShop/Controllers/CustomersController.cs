da64618da1f81fe4c3c07aab3d300fe4af03a3e8
garrett-t <LANDONG@ETSU.EDU>
2020-02-19 21:50:07 -0500

    customer views and api methods

---
+++ BikeShop/BikeShop/Controllers/CustomersController.cs
@@ -0,0 +1,126 @@
+﻿using BikeShop.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BikeShop.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            IEnumerable<CustomerViewModel> c = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/");
+                var responseTask = client.GetAsync("customer");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<CustomerViewModel>>();
+                    readTask.Wait();
+
+                    c = readTask.Result;
+                }
+                else
+                {
+                    c = Enumerable.Empty<CustomerViewModel>();
+                    ModelState.AddModelError(string.Empty, "Server error.");
+                }
+            }
+            return View(c);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(CustomerViewModel c)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/customer");
+
+                var postTask = client.PostAsXmlAsync<CustomerViewModel>("customer", c);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Server error.");
+            return View(c);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            CustomerViewModel c = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/");
+
+                var responseTask = client.GetAsync("customer?id=" + id.ToString());
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<CustomerViewModel>();
+                    readTask.Wait();
+
+                    c = readTask.Result;
+                }
+            }
+            return View(c);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(CustomerViewModel c)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/customer");
+
+                var putTask = client.PutAsXmlAsync<CustomerViewModel>("customer", c);
+                putTask.Wait();
+
+                var result = putTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(c);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/customer");
+
+                var deleteTask = client.DeleteAsync("customer/" + id.ToString());
+                deleteTask.Wait();
+
+                var result = deleteTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}

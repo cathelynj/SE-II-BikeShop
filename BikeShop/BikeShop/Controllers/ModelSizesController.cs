d32cb64c20fe69ec3d7f9c98417c31260169454c
garrett-t <LANDONG@ETSU.EDU>
2020-02-19 22:52:32 -0500

    Letterstyle and modelsize views and api methods

---
+++ BikeShop/BikeShop/Controllers/ModelSizesController.cs
@@ -0,0 +1,126 @@
+﻿using BikeShop.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BikeShop.Controllers
+{
+    public class ModelSizesController : Controller
+    {
+        // GET: ModelSizes
+        public ActionResult Index()
+        {
+            IEnumerable<ModelSizeViewModel> ms = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/");
+                var responseTask = client.GetAsync("modelsize");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<ModelSizeViewModel>>();
+                    readTask.Wait();
+
+                    ms = readTask.Result;
+                }
+                else
+                {
+                    ms = Enumerable.Empty<ModelSizeViewModel>();
+                    ModelState.AddModelError(string.Empty, "Server error.");
+                }
+            }
+            return View(ms);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ModelSizeViewModel ms)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/modelsize");
+
+                var postTask = client.PostAsXmlAsync<ModelSizeViewModel>("modelsize", ms);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Server error.");
+            return View(ms);
+        }
+
+        public ActionResult Edit(string type, int size)
+        {
+            ModelSizeViewModel ms = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/");
+
+                var responseTask = client.GetAsync("modelsize?type=" + type.ToString() + "&size=" + size.ToString());
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<ModelSizeViewModel>();
+                    readTask.Wait();
+
+                    ms = readTask.Result;
+                }
+            }
+            return View(ms);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ModelSizeViewModel ms)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/modelsize");
+
+                var putTask = client.PutAsXmlAsync<ModelSizeViewModel>("modelsize", ms);
+                putTask.Wait();
+
+                var result = putTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(ms);
+        }
+
+        public ActionResult Delete(string type, int size)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/modelsize");
+
+                var deleteTask = client.DeleteAsync("modelsize/" + type.ToString() + "&" + size.ToString());
+                deleteTask.Wait();
+
+                var result = deleteTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- BikeShop/BikeShop/Controllers/api/ModelSizeController.cs
+++ BikeShop/BikeShop/Controllers/api/ModelSizeController.cs
@@ -35,5 +35,99 @@ namespace BikeShop.Controllers.api
 
             return Ok(modelsize);
         }
+
+        public IHttpActionResult GetModelSize(string type, int size)
+        {
+            ModelSizeViewModel modelSize = null;
+
+            using (var ctx = new BikeShopEntities())
+            {
+                modelSize = ctx.MODELSIZEs
+                    .Where(m => m.MODELTYPE == type && m.MSIZE == size)
+                    .Select(m => new ModelSizeViewModel()
+                    {
+                        MODELTYPE = m.MODELTYPE,
+                        MSIZE = m.MSIZE,
+                        TOPTUBE = m.TOPTUBE,
+                        CHAINSTAY = m.CHAINSTAY,
+                        TOTALLENGTH = m.TOTALLENGTH,
+                        GROUNDCLEARANCE = m.GROUNDCLEARANCE,
+                        HEADTUBEANGLE = m.HEADTUBEANGLE,
+                        SEATTUBEANGLE = m.SEATTUBEANGLE
+                    }).FirstOrDefault<ModelSizeViewModel>();
+            }
+            if (modelSize == null)
+            {
+                return NotFound();
+            }
+            return Ok(modelSize);
+        }
+
+        public IHttpActionResult PostNewModelSize(ModelSizeViewModel m)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            using (var ctx = new BikeShopEntities())
+            {
+                ctx.MODELSIZEs.Add(new MODELSIZE()
+                {
+                    MODELTYPE = m.MODELTYPE,
+                    MSIZE = m.MSIZE,
+                    TOPTUBE = m.TOPTUBE,
+                    CHAINSTAY = m.CHAINSTAY,
+                    TOTALLENGTH = m.TOTALLENGTH,
+                    GROUNDCLEARANCE = m.GROUNDCLEARANCE,
+                    HEADTUBEANGLE = m.HEADTUBEANGLE,
+                    SEATTUBEANGLE = m.SEATTUBEANGLE
+                });
+
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Put(ModelSizeViewModel m)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
+            using (var ctx = new BikeShopEntities())
+            {
+                var existingBP = ctx.MODELSIZEs.Where(w => w.MODELTYPE == m.MODELTYPE && w.MSIZE == m.MSIZE).FirstOrDefault<MODELSIZE>();
+                if (existingBP != null)
+                {
+                    existingBP.MODELTYPE = m.MODELTYPE;
+                    existingBP.MSIZE = m.MSIZE;
+                    existingBP.TOPTUBE = m.TOPTUBE;
+                    existingBP.CHAINSTAY = m.CHAINSTAY;
+                    existingBP.TOTALLENGTH = m.TOTALLENGTH;
+                    existingBP.GROUNDCLEARANCE = m.GROUNDCLEARANCE;
+                    existingBP.HEADTUBEANGLE = m.HEADTUBEANGLE;
+                    existingBP.SEATTUBEANGLE = m.SEATTUBEANGLE;
+
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(string type, int size)
+        {
+
+            using (var ctx = new BikeShopEntities())
+            {
+                var bp = ctx.MODELSIZEs
+                    .Where(w => w.MODELTYPE == type && w.MSIZE == size)
+                    .FirstOrDefault();
+
+                ctx.Entry(bp).State = System.Data.Entity.EntityState.Deleted;
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
     }
 }

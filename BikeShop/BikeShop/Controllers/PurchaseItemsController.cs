ecad77a696332ab1ff878c11b8b2cea3a0c9b9c8
garrett-t <LANDONG@ETSU.EDU>
2020-02-20 00:11:58 -0500

    purchaseitems views and api methods

---
+++ BikeShop/BikeShop/Controllers/PurchaseItemsController.cs
@@ -0,0 +1,126 @@
+﻿using BikeShop.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BikeShop.Controllers
+{
+    public class PurchaseItemsController : Controller
+    {
+        // GET: PurchaseItems
+        public ActionResult Index()
+        {
+            IEnumerable<PurchaseItemViewModel> pi = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/");
+                var responseTask = client.GetAsync("purchaseitem");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<PurchaseItemViewModel>>();
+                    readTask.Wait();
+
+                    pi = readTask.Result;
+                }
+                else
+                {
+                    pi = Enumerable.Empty<PurchaseItemViewModel>();
+                    ModelState.AddModelError(string.Empty, "Server error.");
+                }
+            }
+            return View(pi);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(PurchaseItemViewModel pi)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/purchaseitem");
+
+                var postTask = client.PostAsXmlAsync<PurchaseItemViewModel>("purchaseitem", pi);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Server error.");
+            return View(pi);
+        }
+
+        public ActionResult Edit(int purchaseID, int componentID)
+        {
+            PurchaseItemViewModel pi = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/");
+
+                var responseTask = client.GetAsync("purchaseitem?purchaseID=" + purchaseID.ToString() + "&componentID=" + componentID.ToString());
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<PurchaseItemViewModel>();
+                    readTask.Wait();
+
+                    pi = readTask.Result;
+                }
+            }
+            return View(pi);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(PurchaseItemViewModel pi)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/purchaseitem");
+
+                var putTask = client.PutAsXmlAsync<PurchaseItemViewModel>("purchaseitem", pi);
+                putTask.Wait();
+
+                var result = putTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(pi);
+        }
+
+        public ActionResult Delete(int purchaseID, int componentID)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/purchaseitem");
+
+                var deleteTask = client.DeleteAsync("purchaseitem/" + purchaseID.ToString() + "&" + componentID);
+                deleteTask.Wait();
+
+                var result = deleteTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- BikeShop/BikeShop/Controllers/api/PurchaseItemController.cs
+++ BikeShop/BikeShop/Controllers/api/PurchaseItemController.cs
@@ -32,5 +32,90 @@ namespace BikeShop.Controllers.api
 
             return Ok(purchaseitem);
         }
+
+        public IHttpActionResult GetPurchaseItem(int purchaseID, int componentID)
+        {
+            PurchaseItemViewModel pi = null;
+
+            using (var ctx = new BikeShopEntities())
+            {
+                pi = ctx.PURCHASEITEMs
+                    .Where(p => p.PURCHASEID == purchaseID && p.COMPONENTID == componentID)
+                    .Select(p => new PurchaseItemViewModel()
+                    {
+                        PURCHASEID = p.PURCHASEID,
+                        COMPONENTID = p.COMPONENTID,
+                        PRICEPAID = p.PRICEPAID,
+                        QUANTITY = p.QUANTITY,
+                        QUANTITYRECEIVED = p.QUANTITYRECEIVED
+                    }).FirstOrDefault<PurchaseItemViewModel>();
+            }
+            if (pi == null)
+            {
+                return NotFound();
+            }
+            return Ok(pi);
+        }
+
+        public IHttpActionResult PostNewPurchaseItem(PurchaseItemViewModel p)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            using (var ctx = new BikeShopEntities())
+            {
+                ctx.PURCHASEITEMs.Add(new PURCHASEITEM()
+                {
+                    PURCHASEID = p.PURCHASEID,
+                    COMPONENTID = p.COMPONENTID,
+                    PRICEPAID = p.PRICEPAID,
+                    QUANTITY = p.QUANTITY,
+                    QUANTITYRECEIVED = p.QUANTITYRECEIVED
+                });
+
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Put(PurchaseItemViewModel p)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
+            using (var ctx = new BikeShopEntities())
+            {
+                var existingBP = ctx.PURCHASEITEMs.Where(w => w.PURCHASEID == p.PURCHASEID && w.COMPONENTID == p.COMPONENTID).FirstOrDefault<PURCHASEITEM>();
+                if (existingBP != null)
+                {
+                    existingBP.PURCHASEID = p.PURCHASEID;
+                    existingBP.COMPONENTID = p.COMPONENTID;
+                    existingBP.PRICEPAID = p.PRICEPAID;
+                    existingBP.QUANTITY = p.QUANTITY;
+                    existingBP.QUANTITYRECEIVED = p.QUANTITYRECEIVED;
+
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int purchaseID, int componentID)
+        {
+
+            using (var ctx = new BikeShopEntities())
+            {
+                var bp = ctx.BIKEPARTS
+                    .Where(w => w.SERIALNUMBER == purchaseID && w.COMPONENTID == componentID)
+                    .FirstOrDefault();
+
+                ctx.Entry(bp).State = System.Data.Entity.EntityState.Deleted;
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
     }
 }

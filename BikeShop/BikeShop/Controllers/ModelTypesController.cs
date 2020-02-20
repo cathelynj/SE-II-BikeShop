f25f46e771c8b2f26646ea6ce6dee5889432c675
garrett-t <LANDONG@ETSU.EDU>
2020-02-19 23:07:34 -0500

    modeltypes views and api methods

---
+++ BikeShop/BikeShop/Controllers/ModelTypesController.cs
@@ -0,0 +1,126 @@
+﻿using BikeShop.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BikeShop.Controllers
+{
+    public class ModelTypesController : Controller
+    {
+        // GET: ModelTypes
+        public ActionResult Index()
+        {
+            IEnumerable<ModelTypeViewModel> mt = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/");
+                var responseTask = client.GetAsync("modeltype");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<ModelTypeViewModel>>();
+                    readTask.Wait();
+
+                    mt = readTask.Result;
+                }
+                else
+                {
+                    mt = Enumerable.Empty<ModelTypeViewModel>();
+                    ModelState.AddModelError(string.Empty, "Server error.");
+                }
+            }
+            return View(mt);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ModelTypeViewModel mt)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/modeltype");
+
+                var postTask = client.PostAsXmlAsync<ModelTypeViewModel>("modeltype", mt);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Server error.");
+            return View(mt);
+        }
+
+        public ActionResult Edit(string type)
+        {
+            ModelTypeViewModel mt = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/");
+
+                var responseTask = client.GetAsync("modeltype?type=" + type.ToString());
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<ModelTypeViewModel>();
+                    readTask.Wait();
+
+                    mt = readTask.Result;
+                }
+            }
+            return View(mt);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(ModelTypeViewModel mt)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/modeltype");
+
+                var putTask = client.PutAsXmlAsync<ModelTypeViewModel>("modeltyp", mt);
+                putTask.Wait();
+
+                var result = putTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(mt);
+        }
+
+        public ActionResult Delete(string type)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/modeltype");
+
+                var deleteTask = client.DeleteAsync("modeltype/" + type.ToString());
+                deleteTask.Wait();
+
+                var result = deleteTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- BikeShop/BikeShop/Controllers/api/ModelTypeController.cs
+++ BikeShop/BikeShop/Controllers/api/ModelTypeController.cs
@@ -30,5 +30,84 @@ namespace BikeShop.Controllers.api
 
             return Ok(modeltype);
         }
+
+        public IHttpActionResult GetModelType(string type)
+        {
+            ModelTypeViewModel mt = null;
+
+            using (var ctx = new BikeShopEntities())
+            {
+                mt = ctx.MODELTYPEs
+                    .Where(m => m.MODELTYPE1 == type)
+                    .Select(m => new ModelTypeViewModel()
+                    {
+                        MODELTYPE1 = m.MODELTYPE1,
+                        DESCRIPTION = m.DESCRIPTION,
+                        COMPONENTID = m.COMPONENTID
+                    }).FirstOrDefault<ModelTypeViewModel>();
+            }
+            if (mt == null)
+            {
+                return NotFound();
+            }
+            return Ok(mt);
+        }
+
+        public IHttpActionResult PostNewBikePart(ModelTypeViewModel m)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            using (var ctx = new BikeShopEntities())
+            {
+                ctx.MODELTYPEs.Add(new MODELTYPE()
+                {
+                    MODELTYPE1 = m.MODELTYPE1,
+                    DESCRIPTION = m.DESCRIPTION,
+                    COMPONENTID = m.COMPONENTID
+                });
+
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Put(ModelTypeViewModel m)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
+            using (var ctx = new BikeShopEntities())
+            {
+                var existingBP = ctx.MODELTYPEs.Where(w => w.MODELTYPE1 == m.MODELTYPE1).FirstOrDefault<MODELTYPE>();
+                if (existingBP != null)
+                {
+                    existingBP.MODELTYPE1 = m.MODELTYPE1;
+                    existingBP.DESCRIPTION = m.DESCRIPTION;
+                    existingBP.COMPONENTID = m.COMPONENTID;
+
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(string type)
+        {
+
+            using (var ctx = new BikeShopEntities())
+            {
+                var bp = ctx.MODELTYPEs
+                    .Where(w => w.MODELTYPE1 == type)
+                    .FirstOrDefault();
+
+                ctx.Entry(bp).State = System.Data.Entity.EntityState.Deleted;
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
     }
 }

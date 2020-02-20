ec7a5f20ea6fbfc88e73dd74c234c5f7a5fa2a8a
garrett-t <LANDONG@ETSU.EDU>
2020-02-20 10:46:34 -0500

    statetaxrate views and api methods

--- BikeShop/BikeShop/Controllers/api/StateTaxRateController.cs
+++ BikeShop/BikeShop/Controllers/api/StateTaxRateController.cs
@@ -29,5 +29,81 @@ namespace BikeShop.Controllers.api
 
             return Ok(taxrate);
         }
+
+        public IHttpActionResult GetStateTaxRate(string id)
+        {
+            StateTaxRateViewModel bikeParts = null;
+
+            using (var ctx = new BikeShopEntities())
+            {
+                bikeParts = ctx.STATETAXRATEs
+                    .Where(s => s.STATE == id)
+                    .Select(s => new StateTaxRateViewModel()
+                    {
+                        STATE = s.STATE,
+                        TAXRATE = s.TAXRATE
+                    }).FirstOrDefault<StateTaxRateViewModel>();
+            }
+            if (bikeParts == null)
+            {
+                return NotFound();
+            }
+            return Ok(bikeParts);
+        }
+
+        public IHttpActionResult PostNewStateTaxRate(StateTaxRateViewModel s)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            using (var ctx = new BikeShopEntities())
+            {
+                ctx.STATETAXRATEs.Add(new STATETAXRATE()
+                {
+                    STATE = s.STATE,
+                    TAXRATE = s.TAXRATE
+                });
+
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Put(StateTaxRateViewModel s)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
+            using (var ctx = new BikeShopEntities())
+            {
+                var existingBP = ctx.STATETAXRATEs.Where(w => w.STATE == s.STATE).FirstOrDefault<STATETAXRATE>();
+                if (existingBP != null)
+                {
+                    existingBP.STATE = s.STATE;
+                    existingBP.TAXRATE = s.TAXRATE;
+
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(string id)
+        {
+
+            using (var ctx = new BikeShopEntities())
+            {
+                var bp = ctx.STATETAXRATEs
+                    .Where(w => w.STATE == id)
+                    .FirstOrDefault();
+
+                ctx.Entry(bp).State = System.Data.Entity.EntityState.Deleted;
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
     }
 }

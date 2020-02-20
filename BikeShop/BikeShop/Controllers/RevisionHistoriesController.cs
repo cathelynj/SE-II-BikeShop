c4f2a2fd45de46e2bd9c8374993b84968d417ebf
garrett-t <LANDONG@ETSU.EDU>
2020-02-20 01:11:02 -0500

    revisionhistory views and api methods

---
+++ BikeShop/BikeShop/Controllers/RevisionHistoriesController.cs
@@ -0,0 +1,126 @@
+﻿using BikeShop.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BikeShop.Controllers
+{
+    public class RevisionHistoriesController : Controller
+    {
+        // GET: RevisionHistories
+        public ActionResult Index()
+        {
+            IEnumerable<RevisionHistoryViewModel> r = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/");
+                var responseTask = client.GetAsync("revisionhistory");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<RevisionHistoryViewModel>>();
+                    readTask.Wait();
+
+                    r = readTask.Result;
+                }
+                else
+                {
+                    r = Enumerable.Empty<RevisionHistoryViewModel>();
+                    ModelState.AddModelError(string.Empty, "Server error.");
+                }
+            }
+            return View(r);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(RevisionHistoryViewModel r)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/revisionhistory");
+
+                var postTask = client.PostAsXmlAsync<RevisionHistoryViewModel>("revisionhistory", r);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Server error.");
+            return View(r);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            RevisionHistoryViewModel r = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/");
+
+                var responseTask = client.GetAsync("revisionhistory?id=" + id.ToString());
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<RevisionHistoryViewModel>();
+                    readTask.Wait();
+
+                    r = readTask.Result;
+                }
+            }
+            return View(r);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(RevisionHistoryViewModel r)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/revisionhistory");
+
+                var putTask = client.PutAsXmlAsync<RevisionHistoryViewModel>("revisionhistory", r);
+                putTask.Wait();
+
+                var result = putTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(r);
+        }
+
+        public ActionResult Delete(int id)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/revisionhistory");
+
+                var deleteTask = client.DeleteAsync("revisionhistory/" + id.ToString());
+                deleteTask.Wait();
+
+                var result = deleteTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- BikeShop/BikeShop/Controllers/api/RevisionHistoryController.cs
+++ BikeShop/BikeShop/Controllers/api/RevisionHistoryController.cs
@@ -32,5 +32,90 @@ namespace BikeShop.Controllers.api
 
             return Ok(revisionhistory);
         }
+
+        public IHttpActionResult GetRevisionHistory(int id)
+        {
+            RevisionHistoryViewModel revisionHistory = null;
+
+            using (var ctx = new BikeShopEntities())
+            {
+                revisionHistory = ctx.REVISIONHISTORies
+                    .Where(r => r.ID == id)
+                    .Select(r => new RevisionHistoryViewModel()
+                    {
+                        ID = r.ID,
+                        VERSION = r.VERSION,
+                        CHANGEDATE = r.CHANGEDATE,
+                        NAME = r.NAME,
+                        REVISIONCOMMENTS = r.REVISIONCOMMENTS
+                    }).FirstOrDefault<RevisionHistoryViewModel>();
+            }
+            if (revisionHistory == null)
+            {
+                return NotFound();
+            }
+            return Ok(revisionHistory);
+        }
+
+        public IHttpActionResult PostNewRevisionHistory(RevisionHistoryViewModel r)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            using (var ctx = new BikeShopEntities())
+            {
+                ctx.REVISIONHISTORies.Add(new REVISIONHISTORY()
+                {
+                    ID = r.ID,
+                    VERSION = r.VERSION,
+                    CHANGEDATE = r.CHANGEDATE,
+                    NAME = r.NAME,
+                    REVISIONCOMMENTS = r.REVISIONCOMMENTS
+                });
+
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Put(RevisionHistoryViewModel r)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
+            using (var ctx = new BikeShopEntities())
+            {
+                var existingBP = ctx.REVISIONHISTORies.Where(w => w.ID == r.ID).FirstOrDefault<REVISIONHISTORY>();
+                if (existingBP != null)
+                {
+                    existingBP.ID = r.ID;
+                    existingBP.VERSION = r.VERSION;
+                    existingBP.CHANGEDATE = r.CHANGEDATE;
+                    existingBP.NAME = r.NAME;
+                    existingBP.REVISIONCOMMENTS = r.REVISIONCOMMENTS;
+
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+
+            using (var ctx = new BikeShopEntities())
+            {
+                var bp = ctx.REVISIONHISTORies
+                    .Where(w => w.ID == id)
+                    .FirstOrDefault();
+
+                ctx.Entry(bp).State = System.Data.Entity.EntityState.Deleted;
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
     }
 }

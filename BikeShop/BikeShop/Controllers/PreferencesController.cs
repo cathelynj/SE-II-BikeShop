ec77a723b6c55acbc8635d71e013d24286271543
garrett-t <LANDONG@ETSU.EDU>
2020-02-19 23:52:55 -0500

    preference views and api methods

---
+++ BikeShop/BikeShop/Controllers/PreferencesController.cs
@@ -0,0 +1,126 @@
+﻿using BikeShop.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BikeShop.Controllers
+{
+    public class PreferencesController : Controller
+    {
+        // GET: Preferences
+        public ActionResult Index()
+        {
+            IEnumerable<PreferenceViewModel> p = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/");
+                var responseTask = client.GetAsync("preference");
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<IList<PreferenceViewModel>>();
+                    readTask.Wait();
+
+                    p = readTask.Result;
+                }
+                else
+                {
+                    p = Enumerable.Empty<PreferenceViewModel>();
+                    ModelState.AddModelError(string.Empty, "Server error.");
+                }
+            }
+            return View(p);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(PreferenceViewModel p)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/preference");
+
+                var postTask = client.PostAsXmlAsync<PreferenceViewModel>("preference", p);
+                postTask.Wait();
+
+                var result = postTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            ModelState.AddModelError(string.Empty, "Server error.");
+            return View(p);
+        }
+
+        public ActionResult Edit(string item)
+        {
+            PreferenceViewModel p = null;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/");
+
+                var responseTask = client.GetAsync("preference?item=" + item.ToString());
+                responseTask.Wait();
+
+                var result = responseTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<PreferenceViewModel>();
+                    readTask.Wait();
+
+                    p = readTask.Result;
+                }
+            }
+            return View(p);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(PreferenceViewModel p)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/preference");
+
+                var putTask = client.PutAsXmlAsync<PreferenceViewModel>("preference", p);
+                putTask.Wait();
+
+                var result = putTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(p);
+        }
+
+        public ActionResult Delete(string item)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:51968/api/preference");
+
+                var deleteTask = client.DeleteAsync("preference/" + item.ToString());
+                deleteTask.Wait();
+
+                var result = deleteTask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
--- BikeShop/BikeShop/Controllers/api/PreferenceController.cs
+++ BikeShop/BikeShop/Controllers/api/PreferenceController.cs
@@ -13,7 +13,7 @@ namespace BikeShop.Controllers.api
     {
         public class GroupOController : ApiController
         {
-            public IHttpActionResult GetAllGroupO()
+            public IHttpActionResult GetAllPreferences()
             {
                 IList<PreferenceViewModel> preference = null;
                 using (var ctx = new BikeShopEntities())
@@ -33,6 +33,88 @@ namespace BikeShop.Controllers.api
                 }
 
                 return Ok(preference);
+            }
+
+            public IHttpActionResult GetPreference(string item)
+            {
+                PreferenceViewModel bikeParts = null;
+
+                using (var ctx = new BikeShopEntities())
+                {
+                    bikeParts = ctx.PREFERENCEs
+                        .Where(c => c.ITEMNAME == item)
+                        .Select(c => new PreferenceViewModel()
+                        {
+                            ItemName = c.ITEMNAME,
+                            Value = c.VALUE,
+                            Description = c.DESCRIPTION,
+                            DateChanged = c.DATECHANGED
+                        }).FirstOrDefault<PreferenceViewModel>();
+                }
+                if (bikeParts == null)
+                {
+                    return NotFound();
+                }
+                return Ok(bikeParts);
+            }
+
+            public IHttpActionResult PostNewPreference(PreferenceViewModel c)
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest("Invalid data.");
+
+                using (var ctx = new BikeShopEntities())
+                {
+                    ctx.PREFERENCEs.Add(new PREFERENCE()
+                    {
+                        ITEMNAME = c.ItemName,
+                        VALUE = c.Value,
+                        DESCRIPTION = c.Description,
+                        DATECHANGED = c.DateChanged
+                    });
+
+                    ctx.SaveChanges();
+                }
+                return Ok();
+            }
+
+            public IHttpActionResult Put(PreferenceViewModel c)
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest("Not a valid model");
+                using (var ctx = new BikeShopEntities())
+                {
+                    var existingBP = ctx.PREFERENCEs.Where(w => w.ITEMNAME == c.ItemName).FirstOrDefault<PREFERENCE>();
+                    if (existingBP != null)
+                    {
+                        existingBP.ITEMNAME = c.ItemName;
+                        existingBP.VALUE = c.Value;
+                        existingBP.DESCRIPTION = c.Description;
+                        existingBP.DATECHANGED = c.DateChanged;
+
+                        ctx.SaveChanges();
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+                return Ok();
+            }
+
+            public IHttpActionResult Delete(string item)
+            {
+
+                using (var ctx = new BikeShopEntities())
+                {
+                    var bp = ctx.PREFERENCEs
+                        .Where(w => w.ITEMNAME == item)
+                        .FirstOrDefault();
+
+                    ctx.Entry(bp).State = System.Data.Entity.EntityState.Deleted;
+                    ctx.SaveChanges();
+                }
+                return Ok();
             }
         }
     }

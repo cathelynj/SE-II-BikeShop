53ab7a784d64c6bb355581809ab358c80b20f4d4
garrett-t <LANDONG@ETSU.EDU>
2020-02-19 19:51:40 -0500

    workarea views and api methods

--- BikeShop/BikeShop/Controllers/api/WorkAreaController.cs
+++ BikeShop/BikeShop/Controllers/api/WorkAreaController.cs
@@ -29,5 +29,81 @@ namespace BikeShop.Controllers.api
 
             return Ok(workareas);
         }
+
+        public IHttpActionResult GetWorkArea(string id)
+        {
+            WorkAreaViewModel workarea = null;
+
+            using (var ctx = new BikeShopEntities())
+            {
+                workarea = ctx.WORKAREAs
+                    .Where(w => w.WORKAREA1 == id)
+                    .Select(w => new WorkAreaViewModel()
+                    {
+                        WORKAREA1 = w.WORKAREA1,
+                        DESCRIPTION = w.DESCRIPTION
+                    }).FirstOrDefault<WorkAreaViewModel>();
+            }
+            if (workarea == null)
+            {
+                return NotFound();
+            }
+            return Ok(workarea);
+        }
+
+        public IHttpActionResult PostNewWorkArea(WorkAreaViewModel w)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            using (var ctx = new BikeShopEntities())
+            {
+                ctx.WORKAREAs.Add(new WORKAREA()
+                {
+                    WORKAREA1 = w.WORKAREA1,
+                    DESCRIPTION = w.DESCRIPTION
+                });
+
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Put(WorkAreaViewModel workArea)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
+            using (var ctx = new BikeShopEntities())
+            {
+                var existingWA = ctx.WORKAREAs.Where(w => w.WORKAREA1 == workArea.WORKAREA1).FirstOrDefault<WORKAREA>();
+                if (existingWA != null)
+                {
+                    existingWA.WORKAREA1 = workArea.WORKAREA1;
+                    existingWA.DESCRIPTION = workArea.DESCRIPTION;
+
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(string id)
+        {
+           
+            using (var ctx = new BikeShopEntities())
+            {
+                var workArea = ctx.WORKAREAs
+                    .Where(w => w.WORKAREA1 == id)
+                    .FirstOrDefault();
+
+                ctx.Entry(workArea).State = System.Data.Entity.EntityState.Deleted;
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
     }
 }
--- BikeShop/BikeShop/Models/ViewModels/WorkAreaViewModel.cs
+++ BikeShop/BikeShop/Models/ViewModels/WorkAreaViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +8,7 @@ namespace BikeShop.Models.ViewModels
 {
     public class WorkAreaViewModel
     {
+        [Key]
         public string WORKAREA1 { get; set; }
         public string DESCRIPTION { get; set; }
     }

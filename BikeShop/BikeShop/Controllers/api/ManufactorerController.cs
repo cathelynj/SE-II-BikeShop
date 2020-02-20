ce9fd6e2f71dd1b91aa965532a256ba9ff8f9a9b
garrett-t <LANDONG@ETSU.EDU>
2020-02-19 22:17:22 -0500

    manufacturers views and api methods

--- BikeShop/BikeShop/Controllers/api/ManufactorerController.cs
+++ BikeShop/BikeShop/Controllers/api/ManufactorerController.cs
@@ -35,5 +35,99 @@ namespace BikeShop.Controllers.api
 
             return Ok(manufactorer);
         }
+
+        public IHttpActionResult GetBikePart(int id)
+        {
+            ManufactorerViewModel man = null;
+
+            using (var ctx = new BikeShopEntities())
+            {
+                man = ctx.MANUFACTURERs
+                    .Where(m => m.MANUFACTURERID == id)
+                    .Select(m => new ManufactorerViewModel()
+                    {
+                        MANUFACTURERID = m.MANUFACTURERID,
+                        MANUFACTURERNAME = m.MANUFACTURERNAME,
+                        CONTACTNAME = m.CONTACTNAME,
+                        PHONE = m.PHONE,
+                        ADDRESS = m.ADDRESS,
+                        ZIPCODE = m.ZIPCODE,
+                        CITYID = m.CITYID,
+                        BALANCEDUE = m.BALANCEDUE
+                    }).FirstOrDefault<ManufactorerViewModel>();
+            }
+            if (man == null)
+            {
+                return NotFound();
+            }
+            return Ok(man);
+        }
+
+        public IHttpActionResult PostNewBikePart(ManufactorerViewModel m)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Invalid data.");
+
+            using (var ctx = new BikeShopEntities())
+            {
+                ctx.MANUFACTURERs.Add(new MANUFACTURER()
+                {
+                    MANUFACTURERID = m.MANUFACTURERID,
+                    MANUFACTURERNAME = m.MANUFACTURERNAME,
+                    CONTACTNAME = m.CONTACTNAME,
+                    PHONE = m.PHONE,
+                    ADDRESS = m.ADDRESS,
+                    ZIPCODE = m.ZIPCODE,
+                    CITYID = m.CITYID,
+                    BALANCEDUE = m.BALANCEDUE
+                });
+
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Put(ManufactorerViewModel m)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest("Not a valid model");
+            using (var ctx = new BikeShopEntities())
+            {
+                var existingBP = ctx.MANUFACTURERs.Where(w => w.MANUFACTURERID == m.MANUFACTURERID).FirstOrDefault<MANUFACTURER>();
+                if (existingBP != null)
+                {
+                    existingBP.MANUFACTURERID = m.MANUFACTURERID;
+                    existingBP.MANUFACTURERNAME = m.MANUFACTURERNAME;
+                    existingBP.CONTACTNAME = m.CONTACTNAME;
+                    existingBP.PHONE = m.PHONE;
+                    existingBP.ADDRESS = m.ADDRESS;
+                    existingBP.ZIPCODE = m.ZIPCODE;
+                    existingBP.CITYID = m.CITYID;
+                    existingBP.BALANCEDUE = m.BALANCEDUE;
+
+                    ctx.SaveChanges();
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            return Ok();
+        }
+
+        public IHttpActionResult Delete(int id)
+        {
+
+            using (var ctx = new BikeShopEntities())
+            {
+                var bp = ctx.MANUFACTURERs
+                    .Where(w => w.MANUFACTURERID == id)
+                    .FirstOrDefault();
+
+                ctx.Entry(bp).State = System.Data.Entity.EntityState.Deleted;
+                ctx.SaveChanges();
+            }
+            return Ok();
+        }
     }
 }
